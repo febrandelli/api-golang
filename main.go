53cd9788d5d3499b4c9787118c9de85de34f94ca
Felipe Brandelli <fe.brandelli@gmail.com>
2021-05-31 23:51:24 -0300

    implementou gorrila/mux

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Livro struct {
@@ -32,23 +36,33 @@ var livros []Livro = []Livro{
 	},
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bem vindo")
+func pegarId(w http.ResponseWriter, r *http.Request) (int, error) {
+	vars := mux.Vars(r)
+
+	id, err := vars["livroId"]
+
+	if !err {
+		w.WriteHeader(http.StatusBadRequest)
+		return 999, errors.New("Id formato incorreto")
+	}
+
+	return strconv.Atoi(id)
 }
 
 func listarLivros(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Add("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(livros)
 }
 
 func cadastrarLivro(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(201)
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	var novoLivro Livro
@@ -61,25 +75,109 @@ func cadastrarLivro(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(novoLivro)
 }
 
-func routeLivros(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		listarLivros(w, r)
-	} else if r.Method == "POST" {
-		cadastrarLivro(w, r)
+func apagarLivro(w http.ResponseWriter, r *http.Request) {
+	id, err := pegarId(w, r)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	indiceLivro := -1
+	for indice, livro := range livros {
+		if livro.Id == id {
+			indiceLivro = indice
+		}
+	}
+
+	livros = append(livros[0:indiceLivro], livros[indiceLivro+1:]...)
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
-func configHandle() {
-	http.HandleFunc("/", getHello)
-	http.HandleFunc("/livros", routeLivros)
+func modificarLivro(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	id, errId := pegarId(w, r)
+
+	if errId != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	indiceLivro := -1
+	for indice, livro := range livros {
+		if livro.Id == id {
+			indiceLivro = indice
+		}
+	}
+
+	if indiceLivro < 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	body, errBody := ioutil.ReadAll(r.Body)
+	if errBody != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var livroModificado Livro
+	erroJson := json.Unmarshal(body, &livroModificado)
+	if erroJson != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	livroModificado.Id = id
+
+	livros[indiceLivro] = livroModificado
+
+	json.NewEncoder(w).Encode(livros[indiceLivro])
+}
+
+func buscarLivro(w http.ResponseWriter, r *http.Request) {
+
+	id, err := pegarId(w, r)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for _, livro := range livros {
+		if livro.Id == id {
+			json.NewEncoder(w).Encode(livro)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func configHandle(roteador *mux.Router) {
+	roteador.HandleFunc("/livros", listarLivros).Methods("GET")
+	roteador.HandleFunc("/livros/{livroId}", buscarLivro).Methods("GET")
+	roteador.HandleFunc("/livros", cadastrarLivro).Methods("POST")
+	roteador.HandleFunc("/livros/{livroId}", apagarLivro).Methods("DELETE")
+	roteador.HandleFunc("/livros/{livroId}", modificarLivro).Methods("PUT")
+}
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func configServer() {
 	port := "1337"
-	configHandle()
+	roteador := mux.NewRouter().StrictSlash(true)
+	roteador.Use(jsonMiddleware)
+	configHandle(roteador)
 
 	fmt.Println("Rodando na porta " + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, roteador))
 }
 
 func main() {
